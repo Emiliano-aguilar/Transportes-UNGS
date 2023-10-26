17b1ed80caa28b7a17f845e30efd899d103f55d0
emiliano aguilar <1999kamerun@gmail.com>
2023-10-26 16:49:55 -0300

    renames

--- JS/login.js
+++ JS/login.js
@@ -6,7 +6,7 @@ function checkLogin() {
     // Comprueba el usuario y la contraseña predefinidos
     if (username === "admin" && password === "admin") {
         // Redirige a otra página si los datos son correctos
-        window.location.href = "./index.html";
+        window.location.href = "./busqueda_pasajes.html";
     } else {
         // Muestra un mensaje de error si los datos son incorrectos
         errorElement.textContent = "Usuario o contraseña incorrectos";
