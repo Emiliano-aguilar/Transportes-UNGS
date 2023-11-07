600f730b70afb2c56eb896fc0d3482f15e9229e7
sgs31 <agustinaguirre3195@gmail.com>
2023-11-07 13:12:21 -0300

    validaciones en registro

--- JS/registro.js
+++ JS/registro.js
@@ -1,75 +1,80 @@
 document.getElementById("registerButton").addEventListener("click", registerUser);
 
 function registerUser() {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var contrasena = document.getElementById("contrasena").value;
-    var mail = document.getElementById("mail").value;
-    var dni = document.getElementById("dni").value;
-    var errorElement = document.getElementById("error");
+  var nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById("apellido").value;
+  var contrasena = document.getElementById("contrasena").value;
+  var mail = document.getElementById("mail").value;
+  var dni = document.getElementById("dni").value;
+  var errorElement = document.getElementById("error");
+  var isMailValido = validarEmail(mail);
+  var isDNIValido = validarDNI(dni);
+  var isPasswordValido = validarPassword(contrasena);
 
-    // Crear un objeto JSON con los valores
-    const isUsuarioRegistrado = localStorage.getItem(mail);
-    nombre.value = "";
+  if(!isMailValido){
+    errorElement.textContent = "Ingrese un email valido";
+    return;
+  }
 
-    if(isUsuarioRegistrado){
-        errorElement.textContent = "Ya existe un usuario inscripto con el email ingresado"
-        var mail = document.getElementById("mail").value = "";
-        var contrasena = document.getElementById("contrasena").value = "";
-        return;
-    }
+  if(!isPasswordValido){
+    errorElement.textContent = "Ingrese una contraseña entre 5 y 16 valores alfanumericos";
+    return;
+  }
 
-    var usuarioJSON = {
-        nombre: nombre,
-        apellido: apellido,
-        contrasena: contrasena,
-        mail: mail,
-        dni: dni,
-        pasajes: [
-            {
-              "id": "1",
-              "origen": "Buenos Aires",
-              "destino": "Cordoba",
-              "fecha": "2022-05-01",
-              "asiento": "3"
-            },
-            {
-              "id": "2",
-              "origen": "Cordoba",
-              "destino": "Mendoza",
-              "fecha": "2022-06-15",
-              "asiento": "1"
-            },
-            {
-              "id": "3",
-              "origen": "Mendoza",
-              "destino": "Buenos Aires",
-              "fecha": "2022-07-10",
-              "asiento": "5"
-            },
-            {
-              "id": "4",
-              "origen": "Buenos Aires",
-              "destino": "Salta",
-              "fecha": "2022-08-20",
-              "asiento": "2"
-            },
-            {
-              "id": "5",
-              "origen": "Salta",
-              "destino": "Jujuy",
-              "fecha": "2022-09-05",
-              "asiento": "4"
-            }
-          ]
-    };
+  if(!isDNIValido){
+    errorElement.textContent = "Ingrese un DNI valido";
+    return;
+  }
 
-    var usuarioJSONString = JSON.stringify(usuarioJSON);
+  if (nombre === "" || apellido === "" || contrasena === "" || mail === "" || dni === "") {
+    errorElement.textContent = "Todos los campos son obligatorios";
+    return; // Evita que se envíe el formulario si hay campos vacíos
+  }
 
-    // Aquí puedes guardar los datos en una base de datos o realizar otras acciones necesarias.
-    localStorage.setItem(mail, usuarioJSONString);
-    // Muestra una alerta de registro exitoso
-    setTimeout(() => { window.location.href = "./home.html"; }, 2000)
-    alert("Registro exitoso. ¡Bienvenido, " + nombre + " " + apellido + "!");
-    
+  const isUsuarioRegistrado = localStorage.getItem(mail);
+
+  if (isUsuarioRegistrado) {
+    errorElement.textContent = "Ya existe un usuario inscripto con el email ingresado"
+    var mail = document.getElementById("mail").value = "";
+    var contrasena = document.getElementById("contrasena").value = "";
+    return;
+  }
+
+  var usuarioJSON = {
+    nombre: nombre,
+    apellido: apellido,
+    contrasena: contrasena,
+    mail: mail,
+    dni: dni
+  };
+
+  var usuarioJSONString = JSON.stringify(usuarioJSON);
+
+  // Aquí puedes guardar los datos en una base de datos o realizar otras acciones necesarias.
+  localStorage.setItem(mail, usuarioJSONString);
+  // Muestra una alerta de registro exitoso
+  setTimeout(() => { window.location.href = "./home.html"; }, 2000)
+  alert("Registro exitoso. ¡Bienvenido, " + nombre + " " + apellido + "!");
+
+}
+
+function validarEmail(email) {
+  // Expresión regular para verificar si el email tiene un formato válido
+  var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  
+  return regex.test(email);
+}
+
+function validarDNI(dni) {
+  // Expresión regular para verificar si el DNI contiene entre 6 y 8 dígitos numéricos
+  var regex = /^\d{6,8}$/;
+
+  return regex.test(dni);
+}
+
+function validarPassword(password) {
+  // Expresión regular para verificar si la contraseña contiene entre 5 y 16 caracteres alfanuméricos
+  var regex = /^[a-zA-Z0-9]{5,16}$/;
+
+  return regex.test(password);
 }
